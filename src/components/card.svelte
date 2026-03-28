9e0e40ca567e9d4e6512f803116913796ce77489
ozzly <gpg@ozzly.net>
2026-03-28 10:36:34 +0000

    feat: card transitions on hover

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -2,7 +2,9 @@
 	let { img, children } = $props();
 </script>
 
-<div class="grid h-fit w-120 grid-cols-5 overflow-hidden rounded-sm bg-surface0">
+<div
+	class="grid h-fit w-120 grid-cols-5 overflow-hidden rounded-sm bg-surface0 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg"
+>
 	<div class="col-span-2 aspect-7/10 w-full overflow-hidden">
 		<img src={img} alt="alt" class="h-full w-full object-cover object-center" />
 	</div>
