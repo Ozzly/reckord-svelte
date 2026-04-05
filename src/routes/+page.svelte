2228e991bed0aded83bc4bdb3f0fa7ed967eab80
ozzly <gpg@ozzly.net>
2026-04-05 09:17:43 +0100

    feat: add development warning message to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<section class="h-full w-full flex-1">Hello home</section>
+<section class="h-full w-full flex-1">
+	<div class="w-full rounded-lg border border-red bg-red/10 p-4">
+		I'm currently porting Reckord to SvelteKit. This is a work in progress, and the UI is not yet
+		complete. Please expect some bugs and missing features.
+		<br />
+		You can check out the progress at
+		<a
+			href="https://github.com/Ozzly/reckord-svelte"
+			class="text-mauve underline"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Reckord-Svelte</a
+		>.
+	</div>
+</section>
