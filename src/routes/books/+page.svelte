7d899609f1400ad6d9d8343cece185dce6d8b237
ozzly <gpg@ozzly.net>
2026-04-14 12:19:20 +0100

    feat: add warning about using openlibrary

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import { bookToCardData } from '$lib/cardHelpers';
 	import { bookStore } from '$lib/stores/stores.svelte';
 	import type { Book, Status } from '$lib/types';
@@ -13,7 +14,34 @@
 		if (isNaN(value) || value === 0) bookStore.setProgressValue(id, null);
 		else bookStore.setProgressValue(id, value);
 	}
+
+	let hasKey = $state(localStorage.getItem('google_books_api_key') !== null);
+	let warningDismissed = $state(localStorage.getItem('google_books_warning_dismissed') === 'true');
+
+	function dismissWarning() {
+		localStorage.setItem('google_books_warning_dismissed', 'true');
+		warningDismissed = true;
+	}
 </script>
+
+{#if !hasKey && !warningDismissed}
+	<div
+		class="mb-8 flex w-full items-start justify-between rounded-lg border border-peach bg-peach/15 p-1"
+	>
+		<div class="m-3">
+			You haven't set your Google API key yet. Book search can still work without it, by using
+			OpenLibrary's free API, but search results are significantly worse. You can set it
+			<a href={resolve('/settings')} class="text-accent underline">here</a>.
+		</div>
+		<button
+			aria-label="Dismiss Warning"
+			class="ml-4 flex items-center justify-center rounded p-1 text-sm transition-colors hover:bg-peach/19"
+			onclick={dismissWarning}
+		>
+			<span class="icon-[jam--close]"></span>
+		</button>
+	</div>
+{/if}
 
 <div class="flex flex-wrap justify-center gap-8">
 	{#each bookStore.enrichedResults as book (book.id)}
