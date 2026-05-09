8b62b03b3e3f235bd82029d85bf64bb1e2ccf257
ozzly <gpg@ozzly.net>
2026-05-09 10:33:33 +0100

    feat: improve api description + link to usage

--- src/routes/settings/api/+page.svelte
+++ src/routes/settings/api/+page.svelte
@@ -9,13 +9,28 @@
 </script>
 
 <h2 class="font-bold">API Settings</h2>
-<p class="text-subtext1">
-	To provide a useable service out of the box, Reckord will use it's backend as a proxy for API
-	services that require a token. To improve performance, you can provide your own API keys for these
-	services. Reckord will use these keys instead of the default ones. The keys are stored in
-	plaintext in your browser's local storage, so only provide keys that you are comfortable with
-	being stored in this manner.
-</p>
+<div class="text-subtext1">
+	Reckord uses its backend as a proxy for API services by default. Providing your own API keys gives
+	better performance and removes the shared rate limit. Keys are stored in plaintext in your
+	browser's local storage - only add keys you're comfortable storing this way.
+	<div class="mt-2 flex gap-4">
+		<a
+			href="https://github.com/Ozzly/reckord-svelte/blob/main/src/routes/settings/api/%2Bpage.svelte"
+			class="flex items-center text-accent underline"
+			target="_blank"
+			>View how they're stored
+			<span class="icon-[line-md--external-link]"></span></a
+		>
+		<a
+			href="https://github.com/Ozzly/reckord-svelte/blob/main/src/lib/stores/stores.svelte.ts"
+			class="flex items-center text-accent underline"
+			target="_blank"
+		>
+			View how they're used
+			<span class="icon-[line-md--external-link]"></span>
+		</a>
+	</div>
+</div>
 <div class="mt-4 flex px-8">
 	<h3 class="w-1/3">TMDB API Key</h3>
 	<input
